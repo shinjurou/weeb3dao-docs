fc241f9c44874448cf146cde4adc07f1c191b602
Shinjurou <shinjuroukai@gmail.com>
2022-02-13 18:54:34 +0700

    added title for iframe

--- src/Components/EN/8.1 - How to Airdrop an NFT/Airdrop.js
+++ src/Components/EN/8.1 - How to Airdrop an NFT/Airdrop.js
@@ -6,6 +6,7 @@ export default function AirdropNFT() {
       <iframe
         src="https://docs.google.com/document/d/1gMAElC2Yfc2eaxIixpauEuwoJpbMeeD8NNGBSa2hZr4/edit?usp=sharing"
         frameborder="0"
+        title="Polygon Airdrop NFT Guide"
       />
     </main>
   );
