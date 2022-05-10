fd2aeaa72b4ea4a915f5c0bfc93c861a9529749e
ligaratus <ligauwu@protonmail.com>
2022-05-10 20:14:54 +0700

    add scholarship disclaimer

--- src/Components/EN/4 - weeb3dao Mint Scholarship/weeb3daoScholarship.js
+++ src/Components/EN/4 - weeb3dao Mint Scholarship/weeb3daoScholarship.js
@@ -17,8 +17,56 @@ export default function weeb3daoScholarship() {
           style={{ width: "100%", height: "900px" }}
           className="my-form"
         />
+        
         <Typography variant="h3" color="inherit" component="h3" mt={3} mb={2}>
-          List of Artists received the weeb3dao's Mint Scholarship
+          Disclaimer
+        </Typography>
+        <>
+          <ul className="list-disc ml-14 mt-4">
+            <li className="my-2">
+              weeb3dao’s mint scholarship helps artists to get started in NFT. The scholarship is supported by donations and we do not profit from it.  
+            </li>
+            <li className="my-2">
+              We do not judge your art skills or styles and would like to give out opportunities to everyone who can prove that the artworks are of their own creations and not stolen or plagiarized.
+            </li>
+            <li className="my-2">
+              Being accepted is not an endorsement, we are also not responsible for artists who committed acts of plagiarism and copyright infringement after receiving the scholarship. 
+            </li>
+            <li className="my-2">
+              We are in no way accepting acts of plagiarism and copyright infringement. We will give public warning if any offence is committed by scholarship receivers.
+            </li>
+            <li className="my-2">
+              While we would like to give artists second chances as we understand that there are young artists who might not understand the rules of plagiarisms and copyright infringement, we will not accept acts of repeated offence.
+            </li>
+          </ul>
+
+          <Typography variant="h4" color="inherit" component="h4" mt={3} mb={2}>
+            Things that we would like for our scholarship recipients to be cautious of:
+          </Typography>
+          <ul className="list-disc ml-14 mt-4">
+            <li className="my-2">
+              Stealing/tracing and heavy referencing (e.g. pose/character designs) should be avoided.
+            </li>
+            <li className="my-2">
+              Art style imitation is debatable and depends more on the artists themselves. If the artist is okay with it then it is fine, but best to be avoided to prevent potential conflicts.
+            </li>
+            <li className="my-2">
+              Tagging or linking artists/artworks that you are using as inspiration for heavy referencing will still not be acceptable, as most artists will not reply in this scenario. If you cannot provide proofs of the artist agreeing to the work being referenced, then heavy referencing should not be done.
+            </li>
+          </ul>
+
+          <Typography variant="h4" color="inherit" component="h4" mt={3} mb={2}>
+            Resolution:
+          </Typography>
+          <ul className="list-disc ml-14 mt-4">
+            <li className="my-2">
+              Problematic artworks should be burnt. If collected, then the artist should refund the collectors.
+            </li>
+          </ul>
+        </>
+
+        <Typography variant="h3" color="inherit" component="h3" mt={5} mb={2}>
+          List of Artists that have received weeb3dao's Mint Scholarship
         </Typography>
         <Card
           color="green"
--- src/Components/JP/4 - weeb3dao Mint Scholarship/weeb3daoScholarship.js
+++ src/Components/JP/4 - weeb3dao Mint Scholarship/weeb3daoScholarship.js
@@ -17,7 +17,52 @@ export default function weeb3daoScholarship() {
           style={{ width: "100%", height: "900px" }}
           className="my-form"
         />
+
         <Typography variant="h3" color="inherit" component="h3" mt={3} mb={2}>
+          免責事項
+        </Typography>
+        <>
+          <ul className="list-disc ml-14 mt-4">
+            <li className="my-2">
+              weeb3daoのMint Scholarshipは、新しいアーティストがNFTの道を歩み始めることを支援するためのものです。この奨学金は純粋な寄付であり、私たちは奨学金の受給者からいかなる報酬や利益も受け取りません。
+            </li>
+            <li className="my-2">
+              奨学金を受け取った後、盗作や著作権侵害の行為を行ったアーティストに対して、私たちは一切の責任を負いません。
+            </li>
+            <li className="my-2">
+              我々は盗作や著作権侵害の行為を一切認めません。万が一、奨学金受給後に盗作や盗用が発覚した場合は、公開で警告を行います。
+            </li>
+            <li className="my-2">
+              盗作や著作権侵害のルールを理解していない若いアーティストもいるため、セカンドチャンスを与えたいと考えていますが、何度も違反するような行為は認めません。
+            </li>
+          </ul>
+
+          <Typography variant="h4" color="inherit" component="h4" mt={3} mb={2}>
+            奨学生に注意していただきたいこと
+          </Typography>
+          <ul className="list-disc ml-14 mt-4">
+            <li className="my-2">
+              盗用・トレース、大量引用（ポーズ・キャラクターデザインも含む）は行わないでください。
+            </li>
+            <li className="my-2">
+              アートスタイルの模倣についてはグレーゾーンなので、模倣される側のアーティストが了承しているのであるならば問題はありません。ですが、衝突を避けるため、他のアーティストのアートスタイルの模倣は避けることを推奨します。
+            </li>
+            <li className="my-2">
+              また、アーティストをタグ付けしたり、インスピレーションとして使用したアートワークをリンクしたりしても、大半のアーティストは返信をしませんし、リファレンスとして受け入れられません。ですので、アーティストが参照する作品に同意しているということを証明できない場合は、ヘビーリファレンスするべきではありません。
+            </li>
+          </ul>
+
+          <Typography variant="h4" color="inherit" component="h4" mt={3} mb={2}>
+            解決方法
+          </Typography>
+          <ul className="list-disc ml-14 mt-4">
+            <li className="my-2">
+              盗難、トレース、ヘビーリファレンスされた作品は、すべて永久にBurnするべきです。また、作品がすでに回収されている場合は、アーティストが回収者に返金する必要があります。
+            </li>
+          </ul>
+        </>
+        
+        <Typography variant="h3" color="inherit" component="h3" mt={5} mb={2}>
           weeb3dao's Mint 奨学金受賞者リスト
         </Typography>
         <Card
