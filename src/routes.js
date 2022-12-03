aea598d3af60286a311406c3a4724cb2c23630dc
shinjurou <95601144+shinjurou@users.noreply.github.com>
2022-12-04 00:02:07 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const routes = [
   "/info-nft/what-is-blockchain",
   "/info-nft/what-is-digital-wallet",
   "/info-nft/important-terminologies",
-  "security",
+  "/security",
   "/sell-nft",
   "/sell-nft/create-wallet",
   "/sell-nft/choose-blockchain",
