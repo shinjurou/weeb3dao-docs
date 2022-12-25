cb3b0557687ff6a200dfbf5f33daefc781a02c01
shinjurou <shinjuroukai@gmail.com>
2022-12-25 21:15:10 +0700

    Fixed return button in Crypto Exchange

--- src/Components/EN/8 - Crypto Exchange/CryptoExchange.js
+++ src/Components/EN/8 - Crypto Exchange/CryptoExchange.js
@@ -102,8 +102,8 @@ export default function CryptoExchange() {
           </div>
         </div>
         <NaviButtons
-          return="How to Airdrop an NFT"
-          returnlink="/airdrop-NFT"
+          return="How to keep track of Gas"
+          returnlink="/gas"
           next="Community"
           nextlink="/community"
         />
